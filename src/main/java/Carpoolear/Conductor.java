e183060404bc4376e5aee5b6d0e4cded3a62625e
ALEXANDER <1119manuelpoma@gmail.com>
2019-09-12 16:54:25 -0400

    Cambios en los atributos de la clase Conductor, nombre a primerNombre y apellido

--- src/main/java/Carpoolear/Conductor.java
+++ src/main/java/Carpoolear/Conductor.java
@@ -12,13 +12,25 @@ import javax.persistence.Id;
 public class Conductor  {
 
     private @Id @GeneratedValue Long id;
-    private String name;
-    private String nlicensia;
-
-    public Conductor(String name, String nlicensia) {
-        this.name = name;
-        this.nlicensia = nlicensia;
-    }
+    private String primerNombre;
+    private String apellido;
+    private String numeroLiensia;
 
     public Conductor(){}
+
+    public Conductor(String primerNombre, String apellido, String nlicensia) {
+        this.primerNombre = primerNombre;
+        this.apellido = apellido;
+        this.numeroLiensia = nlicensia;
+    }
+
+    public String getNombre(){
+        return this.primerNombre + " " + this.apellido;
+    }
+
+    public void setNombre(String nombre){
+        String[] partes= nombre.split( " ");
+        this.primerNombre = partes[0];
+        this.apellido = partes[1];
+    }
 }

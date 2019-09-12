40e9009b5771c7485ee50dfbb81144fb8421ae42
ALEXANDER <1119manuelpoma@gmail.com>
2019-09-12 18:30:10 -0400

    Actualizacion del cargado de datos de la base de datos

--- src/main/java/Carpoolear/CargarBaseDatos.java
+++ src/main/java/Carpoolear/CargarBaseDatos.java
@@ -11,10 +11,19 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 public class CargarBaseDatos  {
     @Bean
-    CommandLineRunner iniciarBaseDatos(ConductorRepositorio repositorio) {
+    CommandLineRunner iniciarBaseDatos(ConductorRepositorio repositorio,OrdenRespositorio ordenRespositorio) {
+        ordenRespositorio.save(new Orden("Llevar al centro",Estado.EN_PROCESOS));
+        ordenRespositorio.save(new Orden("Dejar en la plaza",Estado.CAMCELADO));
+
+        ordenRespositorio.findAll().forEach(orden -> {
+            log.info("Cargando datos" + orden);
+        });
+
         return args -> {
-            log.info("Cargando datos" + repositorio.save(new Conductor("Miguel Rollano","1458955")));
-            log.info("Cargando datos" + repositorio.save(new Conductor("Wilson Gironda","1168466")));
+            log.info("Cargando datos" + repositorio.save(new Conductor("Miguel" ,"Rollano","1458955")));
+            log.info("Cargando datos" + repositorio.save(new Conductor("Wilson"," Gironda","1168466")));
+
+
 
         };
     }
